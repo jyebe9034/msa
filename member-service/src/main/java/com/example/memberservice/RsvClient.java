126e357455dec3c367ae732f3d64c2c521f62470
jyebe9034 <41553851+jyebe9034@users.noreply.github.com>
2020-01-17 14:14:19 +0900

    Update RsvClient.java

--- member-service/src/main/java/com/example/memberservice/RsvClient.java
+++ member-service/src/main/java/com/example/memberservice/RsvClient.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "rsv-service")
+@FeignClient(name = "rsv-service") // spring.application.name이 rsv-service인 서비스를 호출한다는 의미
 public interface RsvClient {
 
-    @GetMapping("/rsvinfo")
+    @GetMapping("/rsvinfo") // rsv-service모듈 안의 RsvServiceApplication 클래스 안에 있는 rsvinfo()를 호출하는 어노테이션
     Map<String, Object> rsvinfo();
 }
