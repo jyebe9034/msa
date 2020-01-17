5be572cf5a2e81caaf2f5815050143b88aadf890
jyebe9034 <41553851+jyebe9034@users.noreply.github.com>
2020-01-17 14:17:55 +0900

    Update RsvServiceApplication.java

--- rsv-service/src/main/java/com/example/rsvservice/RsvServiceApplication.java
+++ rsv-service/src/main/java/com/example/rsvservice/RsvServiceApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment; // 여러개의 Environment중에 꼭 이걸 import 해야함.
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
-@Slf4j
+@Slf4j // lombok에서 log를 찍을 수 있게 해주는 어노테이션
 public class RsvServiceApplication {
 
 	@Autowired
@@ -22,7 +22,7 @@ public class RsvServiceApplication {
 
 	@GetMapping("/rsvinfo")
 	public ResponseEntity<Map<String, Object>> rsvinfo() {
-		log.info("port: " + environment.getProperty("server.port"));
+		log.info("port: " + environment.getProperty("server.port")); // rsv-service내부의 server.port 설정값을 가져옴.
 		Map<String, Object> map = new HashMap<>();
 		map.put("question", "Have you ever been to Portugal");
 		map.put("answer", "Yes, I've been there four years ago.");
